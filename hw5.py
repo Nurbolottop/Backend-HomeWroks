f1393c33936137497af0015518970d43f8d14c4b
Nurbolottop <erk1nbaev2711@gmail.com>
2022-07-02 20:28:21 +0700

    Домашка

---
+++ hw5.py
@@ -0,0 +1,239 @@
+        #Задача 1
+# Даны два словаря: dictionary_1 = {'a': 300, 'b': 400} и dictionary_2 = {'c': 500, 'd': 600}. 
+# Объедините их в один при помощи встроенных функций языка Python.
+
+# dictionary_1 = {'a': 100, 'b': 200}
+# dictionary_2 = {'x': 300, 'y': 200}
+# print(dictionary_1,dictionary_2)
+# dictionary_1.setdefault("x","300",)
+# dictionary_1.setdefault("y","200")
+# print(dictionary_1)
+
+
+        #Задача 2
+# Дан словарь с числовыми значениями. numbers = {‘num_1’ : 1, ‘num_2’ : 2, ‘num_3’ : 3, ‘num_100’ : 100}
+# Необходимо их все перемножить на 5 и вывести на экран.
+
+# numbers = {'num' : 1, 'num_2': 2, 'num_3' : 3, 'num_100' : 100}
+# c=5
+# for a in numbers:
+#     c=c*numbers[a]
+#     print(res)
+
+
+        #Задача 3
+# Есть словарь adilet = {‘name’ : ‘Adilet’, ‘age’ : 17, ‘color’ : ‘White’}. Умножьте его age на 2 раза 
+
+# adilet = {'name' : 'Adilet', 'age' : 17, 'color' : 'White'}
+# print(adilet)
+# adilet['age'] = 17*2
+# print(adilet)
+    
+
+        #Задача 4
+# Есть словарь adilet = {‘name’ : ‘Adilet’, ‘age’ : 17, ‘color’ : ‘White’}. Удалите ключ и значение color
+
+# adilet = {'name' : 'Adilet', 'age' : 17, 'color' : 'White'}
+# print(adilet)
+# del adilet['color']
+# print(adilet)
+
+
+        #Задача 5
+# Есть словарь adilet = {‘name’ : ‘Adilet’, ‘age’ : 17, ‘color’ : ‘White’}. Обновите age в 16
+
+# adilet = {'name' : 'Adilet', 'age' : 17, 'color' : 'White'}
+# print(adilet)
+# adilet['age'] = 16
+# print(adilet)
+
+
+        #Задача 6
+# Есть словарь adilet = {‘name’ : ‘Adilet’, ‘age’ : 17, ‘color’ : ‘White’}.
+# Добавьте новый ключ(address) и значение(Западный Анар)
+
+# adilet = {'name' : 'Adilet', 'age' : 17, 'color' : 'White'}
+# print(adilet)
+# adilet.setdefault("address","Западный Анар")
+# print(adilet)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hw7.py
@@ -0,0 +1,87 @@
+             # Задача 1
+        # age = int(input("Возраст: ")) 
+        # if age >= 18 and age <= 50: 
+        #     print("Доступ разрешен") 
+        # elif age > 50: 
+        #     print("Вы уже старый") 
+        # else: 
+        #     print("Иди домой") 
+
+# while True:
+#     age = int(input("Возраст: "))
+#     age = (print("Доступ разрешен") if age >= 18 and age <= 50 else print("Вы уже старый"))if age > 50 else(print("Доступ разрешен") if age >= 18 and age <= 50 else print("Иди домой")) 
+#     print(age)
+
+
+             # Задача 2
+    #     my_passwords = ["qwerty", "12345678"] 
+    # user_input = input("Введите пароль: ") 
+    # if user_input in my_passwords: 
+    #     print("Пароль верный") 
+    # else: 
+    #     print("Неверный пароль")
+
+# while True:
+    # my_passwords = ["qwerty", "12345678"]
+    # user_input = input("Введите пароль: ")
+    # us = print("Пароль верный") if user_input in my_passwords else print("Неверный пароль")
+    # print(us)
+
+
+             # Задача 2
+        # name = input("Ваше имя: ") 
+        # names = ["Mark", "Kate", "Anna", "Smit"] 
+        # if name in names: 
+        #     print(f"{name} в нашем классе") 
+        # else: 
+        #     print(f"{name} не в нашем классе")
+
+# while True:
+#     name = input("Ваше имя: ") 
+#     names = ["Mark", "Kate", "Anna", "Smit"]
+
+#     n = print(name,"в нашем классе")if name in names else print(name,  "не в нашем классе")
+#     print(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
